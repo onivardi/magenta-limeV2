59d370a30e7c56fb3046b3a38a3a7133c8159cd9
Valdeir Rodrigues <vardii.rodrigues@gmail.com>
2024-04-26 07:49:21 -0400

    add colors and font to base layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lime: '#A4FFA2',
+        magenta: '#F47DDA',
+        magenta50: '#F47DDA80',
+        tan: '#CEC5B4',
+        teal: '#2E6D81',
+        cyan: '#7DE6F4',
+        black: '#050D14',
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
